8ae2f6777f1807d2f3da02f62a8955bd1de08204
Zhenyu Yang <50858833+yzyjsjem@users.noreply.github.com>
2020-05-27 00:02:44 +1000

    改动位置

--- startstations.sh
+++
@@ -1,6 +0,0 @@
-./station BusportA 4001 4002 4008 4010 &
-./station StationB 4003 4004 4008 &
-./station TerminalC 4005 4006 4010 &
-./station JunctionD 4007 4008 4002 4004 4010 4012 &
-./station BusportE 4009 4010 4002 4006 4008 4012 &
-./station StationF 4011 4012 4008 4010 &
